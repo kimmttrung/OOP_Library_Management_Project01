89dd6f72e48ef99746c6f523511984c46a4be1cd
Thế Pháp <phapok0@gmail.com>
2024-10-05 15:26:51 +0700

    Login

--- src/main/java/management/libarymanagement/LibaryManagement.java
+++ src/main/java/management/libarymanagement/LibaryManagement.java
@@ -2,6 +2,7 @@ package management.libarymanagement;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +11,14 @@ import java.io.IOException;
 public class LibaryManagement extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LibaryManagement.class.getResource("/fxml/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Libary Management");
+
         stage.setScene(scene);
+
         stage.show();
     }
 
