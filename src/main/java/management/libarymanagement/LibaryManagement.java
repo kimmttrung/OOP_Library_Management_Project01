a279bb065ef6716a3547e03e857d85fb9b13b5f8
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2024-10-06 10:17:56 +0700

    design login part 1

--- src/main/java/management/libarymanagement/LibaryManagement.java
+++ src/main/java/management/libarymanagement/LibaryManagement.java
@@ -1,23 +1,28 @@
-package management.libarymanagement;
+ package management.libarymanagement;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+ import javafx.application.Application;
+ import javafx.fxml.FXMLLoader;
+ import javafx.scene.Parent;
+ import javafx.scene.Scene;
+ import javafx.stage.Stage;
 
-import java.io.IOException;
+ import java.io.IOException;
 
-public class LibaryManagement extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LibaryManagement.class.getResource("/fxml/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+ public class LibaryManagement extends Application {
+     @Override
+     public void start(Stage stage) throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
 
-    public static void main(String[] args) {
-        launch();
-    }
-}
+         Scene scene = new Scene(root);
+
+         stage.setTitle("Libary Management");
+
+         stage.setScene(scene);
+
+         stage.show();
+     }
+
+     public static void main(String[] args) {
+         launch();
+     }
+ }
