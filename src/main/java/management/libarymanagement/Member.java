1563ab6fafe3bbb5d51cf3e8b2fe1d85e0043301
Thế Pháp <phapok0@gmail.com>
2024-10-10 16:29:40 +0700

    Database

---
+++ src/main/java/management/libarymanagement/Member.java
@@ -0,0 +1,49 @@
+package management.libarymanagement;
+
+import java.sql.Date;
+
+public class Member {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private Date registrationDate;
+
+    public Member(String id, String name, String phoneNumber, Date registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.registrationDate = registrationDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
