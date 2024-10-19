5b89d95faf50ecd501a7bf6b5b857cb8524929d5
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2024-10-19 23:21:56 +0700

    dashBord

--- src/main/java/management/libarymanagement/loginController.java
+++ src/main/java/management/libarymanagement/loginController.java
@@ -6,8 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -34,6 +34,9 @@ public class loginController {
     private Statement statement;
     private ResultSet resultSet;
 
+    private double x;
+    private double y;
+
     @FXML
     public void login() {
         String sql = "SELECT * FROM accounts WHERE username = ? AND password= ?";
@@ -55,16 +58,44 @@ public class loginController {
                 alert.showAndWait();
             } else {
                 if (resultSet.next()) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/test.fxml"));
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("admin Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login");
+                    alert.showAndWait();
+
+                    login_Btn.getScene().getWindow().hide();
+
+                  FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/test.fxml"));
+//                  FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/mainLibrary.fxml"));
+//                  FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashbord.fxml"));
+
+
+//                    Parent root = FXMLLoader.load(getClass().getResource("dashbord.fxml"));
+
                     Parent root = loader.load();
 
-                    Stage currentStage = (Stage) login_Btn.getScene().getWindow();
-                    currentStage.close();
+//                    Stage currentStage = (Stage) login_Btn.getScene().getWindow();
+//                    currentStage.close();
 
                     Stage stage = new Stage();
                     Scene scene = new Scene(root);
+
+                    root.setOnMousePressed((javafx.scene.input.MouseEvent e) -> {
+                        x = e.getSceneX();
+                        y = e.getSceneY();
+                    });
+
+                    root.setOnMouseDragged((javafx.scene.input.MouseEvent e) -> {
+                        stage.setX(e.getScreenX() - x);
+                        stage.setY(e.getScreenY() - y);
+                    });
+
+                    stage.initStyle(StageStyle.TRANSPARENT);
+
                     stage.setScene(scene);
                     stage.show();
+
                 } else {
                     alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module management.libarymanagement {
     requires javafx.fxml;
     requires java.sql;
     requires com.google.gson;
+    requires java.desktop;
 
     opens management.libarymanagement to javafx.fxml;
     exports management.libarymanagement;
+    exports Controller;
+    opens Controller to javafx.fxml;
 }
